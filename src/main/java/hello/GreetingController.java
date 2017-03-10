8e8f174072dacb3fd3887bf3e96ab9b5d0edc44a
= <=>
2017-03-10 22:29:54 +0800

    first submited

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,41 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @CrossOrigin(origins = "http://localhost:9000")
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
+        System.out.println("==== in greeting ====");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/greeting-javaconfig")
+    public Greeting greetingWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
+        System.out.println("==== in greeting ====");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+    
+    
+    @RequestMapping("/linbin")
+    @ResponseBody
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
